d569d151783aa6ce053139d4f678200d4cdef01c
GFssp <gfssp_2041@proton.me>
2026-02-15 20:44:24 -0300

    update: consumer - rabbitmq

--- worker/cmd/main.go
+++ worker/cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	sabbitmq "govision_worker/internal/services/rabbitmq"
 	"log"
 	"os"
@@ -8,11 +10,19 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main(){
+func main() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
-	_ = godotenv.Load()
 
-	rabbitMQConnection, err := sabbitmq.NewRabbittMQConnection(os.Getenv("RABBITMQ_URL"))
+	_ = godotenv.Load()
+	rabbitConnString := os.Getenv("RABBITMQ_URL")
+	rabbitQueueString := os.Getenv("RABBITMQ_QUEUE")
+
+	if rabbitConnString == "" || rabbitQueueString == "" {
+		log.Printf("[ERROR] - Environment variables no found.")
+		panic(errors.New("Environment variables not found."))
+	}
+
+	rabbitMQConnection, err := sabbitmq.NewRabbittMQConnection(rabbitConnString)
 	if err != nil {
 		log.Printf("[ERROR] - RabbitMQ connection error: %v", err)
 		panic(err)
@@ -25,4 +35,25 @@ func main(){
 		panic(err)
 	}
 	defer ch.Close()
+
+	msgs, err := ch.Consume(
+		rabbitQueueString,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	forever := make(chan bool)
+	go func() {
+		for d := range msgs {
+			fmt.Printf("Received message: %s\n", d.Body)
+		}
+	}()
+
+	fmt.Println("Successfully connected to RabbitMQ instance")
+	fmt.Println("[*] - Waiting for messages")
+	<-forever
 }
